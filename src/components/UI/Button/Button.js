63acdacb88894cd8c1df8408a7aacba0717c49bd
mrsnailo <shahidparvez.official@gmail.com>
2023-04-14 22:38:32 +0600

    restyled button component with styled-components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,31 @@
-import React from "react";
+import styled from "styled-components";
 
-import "./Button.css";
 
-const Button = (props) => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button`
+  width: 150px;
+  padding: 10px 5px;
+  background: #b7102d;
+  color: #ffffff;
+  font-weight: 600;
+  border-style: none;
+  border-radius: 8px;
+&:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+  background: tomato;
+border: 1px solid orange;
+}
+
+`;
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+//};
 
 export default Button;
